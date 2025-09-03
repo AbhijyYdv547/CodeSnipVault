c6d951c9935b8a67de73045d69555552ee9301a7
Abhijay Yadav <yadavabhijay@gmail.com>
2025-09-03 22:33:43 +0530

    refactor: add chi to main.go

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -2,29 +2,25 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"net/http"
 	"os/signal"
 	"syscall"
 	"time"
 
-	"backend/internal/server"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func gracefulShutdown(apiServer *http.Server, done chan bool) {
-	// Create context that listens for the interrupt signal from the OS.
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
 
-	// Listen for the interrupt signal.
 	<-ctx.Done()
 
 	log.Println("shutting down gracefully, press Ctrl+C again to force")
-	stop() // Allow Ctrl+C to force shutdown
+	stop() 
 
-	// The context is used to inform the server it has 5 seconds to finish
-	// the request it is currently handling
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	if err := apiServer.Shutdown(ctx); err != nil {
@@ -33,26 +29,27 @@ func gracefulShutdown(apiServer *http.Server, done chan bool) {
 
 	log.Println("Server exiting")
 
-	// Notify the main goroutine that the shutdown is complete
 	done <- true
 }
 
 func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello world"))
+	})
 
-	server := server.NewServer()
 
-	// Create a done channel to signal when the shutdown is complete
 	done := make(chan bool, 1)
+	server := &http.Server{Addr: ":8000", Handler: r}
 
-	// Run graceful shutdown in a separate goroutine
 	go gracefulShutdown(server, done)
 
 	err := server.ListenAndServe()
 	if err != nil && err != http.ErrServerClosed {
-		panic(fmt.Sprintf("http server error: %s", err))
+		log.Fatal("http server error: ", err)
 	}
 
-	// Wait for the graceful shutdown to complete
 	<-done
 	log.Println("Graceful shutdown complete.")
 }
