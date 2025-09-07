1667d5c38da039ad339a1d680c7d495a18128820
Abhijay Yadav <yadavabhijay@gmail.com>
2025-09-07 18:04:41 +0530

    refactor: add prettier

--- frontend/components/snippets-list.tsx
+++ frontend/components/snippets-list.tsx
@@ -6,40 +6,36 @@ import { useSnippetStore } from "@/store/SnippetStore";
 import SnippetsFilter from "./snippets-filter";
 import SnippetsPagination from "./snippets-pagination";
 
-
 export default function SnippetList() {
-    const [snippets, setSnippets] = useState([])
+  const [snippets, setSnippets] = useState([]);
 
-    const {search,tags,language,page,loading,setLoading} = useSnippetStore();
+  const { search, tags, language, page, loading, setLoading } =
+    useSnippetStore();
 
-    useEffect(() => {
-        setLoading(true)
-        fetchSnippets({ page, search, tags, language })
-            .then((data) => setSnippets(data.data))
-            .catch((err) => console.log(err))
-            .finally(() => setLoading(false))
-    }, [page, search, tags, language,setLoading])
+  useEffect(() => {
+    setLoading(true);
+    fetchSnippets({ page, search, tags, language })
+      .then((data) => setSnippets(data.data))
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false));
+  }, [page, search, tags, language, setLoading]);
 
-
-    return (
-        <div>
-<SnippetsFilter/>
-{loading ? (
-<p>Loading...</p>
-) : (
- (!snippets || snippets.length === 0) ? (
- <p>No snippets available.</p>
- ):(
- <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-                                {snippets.map((snippet, index) => (
-                                    <SnippetCard key={index} snippet={snippet} />
-                                ))}
-                            </div>
-                    )
-
-            )}
-
-            <SnippetsPagination/>
+  return (
+    <div>
+      <SnippetsFilter />
+      {loading ? (
+        <p>Loading...</p>
+      ) : !snippets || snippets.length === 0 ? (
+        <p>No snippets available.</p>
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+          {snippets.map((snippet, index) => (
+            <SnippetCard key={index} snippet={snippet} />
+          ))}
         </div>
-    )
+      )}
+
+      <SnippetsPagination />
+    </div>
+  );
 }
