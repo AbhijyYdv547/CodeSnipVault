9f896756c3d22830b55038be5572f863ca7c461a
Abhijay Yadav <yadavabhijay@gmail.com>
2025-09-07 23:36:25 +0530

    refactor: remove spacing issue

--- frontend/components/DashboardLayout.tsx
+++ frontend/components/DashboardLayout.tsx
@@ -1,40 +1,33 @@
-
 "use client";
 
 import { ReactNode } from "react";
-import { AppSidebar } from "@/components/app-sidebar"
+import { AppSidebar } from "@/components/app-sidebar";
 import {
-    SidebarInset,
-    SidebarProvider,
-    SidebarTrigger,
-} from "@/components/ui/sidebar"
-
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
 
 interface DashboardLayoutProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 
 const DashboardLayout = ({ children }: DashboardLayoutProps) => {
-    return (
-        <div className="flex min-h-screen bg-background">
-            <SidebarProvider>
-                <AppSidebar />
-                <div className="flex flex-col flex-1">
-                <SidebarInset>
-                    <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-                        <div className="flex items-center gap-2 px-4">
-                            <SidebarTrigger className="-ml-1" />
-                        </div>
-                    </header>
-
-                </SidebarInset>
-            <main className="flex-1 px-6">
-                {children} 
-            </main>
+  return (
+    <div className="flex min-h-screen bg-background">
+      <SidebarProvider>
+        <AppSidebar />
+        <div className="flex flex-col flex-1">
+          <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+            <div className="flex items-center gap-2 px-4">
+              <SidebarTrigger className="-ml-1" />
             </div>
-        </SidebarProvider>
+          </header>
+          <main className="flex-1 px-6">{children}</main>
         </div>
-    );
+      </SidebarProvider>
+    </div>
+  );
 };
 
 export default DashboardLayout;
--- frontend/components/snippets-list.tsx
+++ frontend/components/snippets-list.tsx
@@ -28,14 +28,15 @@ export default function SnippetList() {
       ) : !snippets || snippets.length === 0 ? (
         <p>No snippets available.</p>
       ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-          {snippets.map((snippet, index) => (
-            <SnippetCard key={index} snippet={snippet} />
-          ))}
+        <div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+            {snippets.map((snippet, index) => (
+              <SnippetCard key={index} snippet={snippet} />
+            ))}
+          </div>
+          <SnippetsPagination />
         </div>
       )}
-
-      <SnippetsPagination />
     </div>
   );
 }
